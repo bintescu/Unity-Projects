6e86757805a85b2af990d7a0cd45e2e685b4926a
bintescu <41958601+bintescu@users.noreply.github.com>
2020-04-19 20:32:27 +0300

    First commit
    First 2D game

---
+++ Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    // SerializeField pus intre paranteze patrate este folosit pentru a fi vizibil din Inspector
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+
+    private string[] daysOfTheWeek = { "Monday", "Wednessday", "Tuesday", "Thursday", "Friday", "Saturday", "Sunday" };
+    private State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+        Debug.Log(daysOfTheWeek[4]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextstate = state.getNextStates();
+        
+        for(int i = 0; i < nextstate.Length; i++)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextstate[i];
+            }
+        }
+        textComponent.text = state.GetStateStory();
+    }
+}
---
+++ Text101/Assets/Scripts/State.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+   [TextArea(10,14)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] getNextStates() 
+    {
+        return nextStates;
+    }
+    
+}
